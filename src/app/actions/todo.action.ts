12c4b46b85fbb90ea60f0da6cf8100d594aaaeff
Chris Tseng <samfire5200@gmail.com>
2022-08-06 12:21:12 +0800

    ng update @angular/core@12 @angular/cli@12

--- src/app/actions/todo.action.ts
+++ src/app/actions/todo.action.ts
@@ -1,33 +1,25 @@
-import {Todo} from '../models/Todo';
+import { Todo } from "../models/Todo";
 
 export class AddTodo {
-    static readonly type = '[Todo] Add';
-
-    constructor(public payload: Todo) {
-    }
+  static readonly type = "[Todo] Add";
+  constructor(public payload: Todo) {}
 }
 
 export class GetTodos {
-    static readonly type = '[Todo] Get';
+  static readonly type = "[Todo] Get";
 }
 
 export class UpdateTodo {
-    static readonly type = '[Todo] Update';
-
-    constructor(public payload: Todo, public id: number) {
-    }
+  static readonly type = "[Todo] Update";
+  constructor(public payload: Todo, public id: number) {}
 }
 
 export class DeleteTodo {
-    static readonly type = '[Todo] Delete';
-
-    constructor(public id: number) {
-    }
+  static readonly type = "[Todo] Delete";
+  constructor(public id: number) {}
 }
 
 export class SetSelectedTodo {
-    static readonly type = '[Todo] Set';
-
-    constructor(public payload: Todo) {
-    }
+  static readonly type = "[Todo] Set";
+  constructor(public payload: Todo) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,30 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {ReactiveFormsModule} from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import {NgxsModule} from '@ngxs/store';
-import {NgxsReduxDevtoolsPluginModule} from '@ngxs/devtools-plugin';
-import {NgxsLoggerPluginModule} from '@ngxs/logger-plugin';
+import { NgxsModule } from "@ngxs/store";
+import { NgxsReduxDevtoolsPluginModule } from "@ngxs/devtools-plugin";
+import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {ListComponent} from './list/list.component';
-import {FormComponent} from './form/form.component';
-import {TodoState} from './states/todo.state';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ListComponent } from "./list/list.component";
+import { FormComponent } from "./form/form.component";
+import { TodoState } from "./states/todo.state";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        ListComponent,
-        FormComponent
-    ],
-    imports: [
-        BrowserModule,
-        NgxsModule.forRoot([
-            TodoState
-        ]),
-        NgxsReduxDevtoolsPluginModule.forRoot(),
-        NgxsLoggerPluginModule.forRoot(),
-        HttpClientModule,
-        ReactiveFormsModule,
-        AppRoutingModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent, ListComponent, FormComponent],
+  imports: [
+    BrowserModule,
+    NgxsModule.forRoot([TodoState]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
